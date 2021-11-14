f9321467f0b1a0ac8580125cfa556231ab9f67b7
PeterDaniel3D <75070076+PeterDaniel3D@users.noreply.github.com>
2021-11-14 19:24:43 +0100

    initial commit

---
+++ src/main/java/rest/CatsResource.java
@@ -0,0 +1,70 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import utils.EMF_Creator;
+import utils.HttpUtils;
+
+import javax.annotation.security.RolesAllowed;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Path("cat")
+public class CatsResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    Gson GSON = new Gson();
+    @Context
+    private UriInfo context;
+
+    @Context
+    SecurityContext securityContext;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("user")
+    @RolesAllowed("user")
+    public String getCatPicture() throws IOException {
+        return GSON.toJson(HttpUtils.fetchData("https://thatcopy.pw/catapi/rest/"));
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("voice")
+    @RolesAllowed("user")
+    public String getVoiceRSS() throws IOException {
+        JsonObject catFacts = HttpUtils.fetchJson("https://catfact.ninja/fact");
+//                HttpUtils.fetchData("https://catfact.ninja/fact");
+        String fact = String.valueOf(catFacts.get("fact"));
+        fact = fact.replaceAll("\"", ""); // Remove quotations around json element
+
+        List<String> bonusComment = new ArrayList<>();
+        bonusComment.add("Jeez! I don't believe it. These facts are trash!");
+        bonusComment.add("To be honest, I love dogs more than cats!");
+        bonusComment.add("This fact is just pure garbage.");
+        bonusComment.add("Lies. All lies! May I laugh in all eternity!");
+        bonusComment.add("One cat, one hat. Chuck Norris, no cat. Ha ha ha ha. ");
+
+        Random rand = new Random();
+        int randInt = rand.nextInt(bonusComment.size());
+
+        String json = "{\"url\":\"http://api.voicerss.org/?key=4a3189fb0bf34912aec4aea4873d90b0&hl=en-gb&v=Alice&c=MP3&f=16khz_16bit_stereo&src=" + fact + " " + bonusComment.get(randInt) + ".\", \"fact\":\"" + fact + "\"}";
+        return GSON.toJson(json);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("admin")
+    @RolesAllowed("admin")
+    public String getCatPictureForAdmin() throws IOException {
+        //DOGS IN A CAT ENDPOINT??? WHAT KIND DARK MAGIC IS THIS??
+        return GSON.toJson(HttpUtils.fetchData("https://dog-api.kinduff.com/api/facts"));
+    }
+}
---
+++ src/main/java/utils/HttpUtils.java
@@ -0,0 +1,75 @@
+package utils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+//import dtos.ChuckDTO;
+//import dtos.CombinedDTO;
+//import dtos.DadDTO;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class HttpUtils {
+    private static Gson gson = new Gson();
+
+//    public static CombinedDTO fetchDataSequential() throws IOException {
+//        String chuck = HttpUtils.fetchData("https://api.chucknorris.io/jokes/random");
+//        String dad = HttpUtils.fetchData("https://icanhazdadjoke.com");
+//
+//        ChuckDTO chuckDTO = gson.fromJson(chuck, ChuckDTO.class);
+//        DadDTO dadDTO = gson.fromJson(dad, DadDTO.class);
+//        return new CombinedDTO(chuckDTO,dadDTO);
+//    }
+//
+//    public static CombinedDTO fetchDataParrallel() throws ExecutionException, InterruptedException, TimeoutException {
+//        ExecutorService es = Executors.newCachedThreadPool();
+//        Future<ChuckDTO> chuckDTOFuture = es.submit(
+//                () -> gson.fromJson(HttpUtils.fetchData("https://api.chucknorris.io/jokes/random"),ChuckDTO.class)
+//        );
+//        Future<DadDTO> dadDTOFuture = es.submit(
+//                () -> gson.fromJson(HttpUtils.fetchData("https://icanhazdadjoke.com"),DadDTO.class)
+//        );
+//
+//        ChuckDTO chuckDTO = chuckDTOFuture.get();
+//        DadDTO dadDTO = dadDTOFuture.get();
+//
+//        return new CombinedDTO(chuckDTO, dadDTO);
+//    }
+
+
+    public static String fetchData(String _url) throws MalformedURLException, IOException {
+        URL url = new URL(_url);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        //con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestProperty("User-Agent", "server");
+
+        Scanner scan = new Scanner(con.getInputStream());
+        String jsonStr = null;
+        if (scan.hasNext()) {
+            jsonStr = scan.nextLine();
+        }
+        scan.close();
+        return jsonStr;
+    }
+
+    public static JsonObject fetchJson(String _url) throws IOException {
+        URL url = new URL(_url);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject jsonobj = root.getAsJsonObject();
+        return jsonobj;
+    }
+}
